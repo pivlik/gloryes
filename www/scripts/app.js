78be77647fae7049c41d9f37837a3e5ea4d64c19
pivlik <p.klimin@yandex.ru>
2017-11-06 20:19:43 +0300

    window for add basket ready

--- www/scripts/app.js
+++ www/scripts/app.js
@@ -199,6 +199,24 @@ define('app', ['jquery', 'slick-carousel', 'magnific-popup'], function ($) {
         });
     })('.image-popup-fit-width');
 
+    //Увеличение выбранной картинки...............................
+    $(document).ready(function() {
+        $('.popup-with-zoom-anim').magnificPopup({
+            type: 'inline',
+
+            fixedContentPos: false,
+            fixedBgPos: true,
+
+            overflowY: 'auto',
+
+            closeBtnInside: true,
+            preloader: false,
+
+            midClick: true,
+            removalDelay: 300,
+            mainClass: 'my-mfp-zoom-in'
+        });
+    });
     // Стилизация селекта
     (function ($selects) {
         if (!$selects.length) {
